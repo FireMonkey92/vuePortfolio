130e129e1929e0245a2b57a64b9212bd531a7d52
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2021-06-30 16:50:19 +0530

    Added new Example

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -6,9 +6,15 @@
     <Services />
     <Skills :isDarkMode="isDarkMode ? isDarkMode : false" />
     <Team />
-    <a id="download">Download</a>
-<button id="stop">Stop</button>
-
+     <h1> MediaRecorder API example</h1>
+                                    <p> For now it is supported only in Firefox(v25+) and Chrome(v47+)</p>
+                                    <div id='btns'>
+                                        <button className="btn btn-default mr-3" id='start'>Start</button>
+                                        <button className="btn btn-default" id='stop'>Stop</button>
+                                    </div>
+                                    <div>
+                                        <ul className="list-unstyled" id='ul'></ul>
+                                    </div>
     <Contact :isDarkMode="isDarkMode ? isDarkMode : false" />
     <Footer :isDarkMode="isDarkMode ? isDarkMode : false" />
   </div>
@@ -80,46 +86,76 @@ export default {
       // applying again smooth scroll on menu items click
       $("html").css("scrollBehavior", "smooth");
     });
-    
-    let shouldStop = false;
-  let stopped = false;
-  const downloadLink = document.getElementById('download');
-  const stopButton = document.getElementById('stop');
+  
+  //VOICE RECORDING API
+  
+  // DOM and Variables
+        let log = console.log.bind(console),
+            id = val => document.getElementById(val),
+            ul = id('ul'),
+            start = id('start'),
+            stop = id('stop'),
+            stream,
+            recorder,
+            counter = 1,
+            chunks,
+            media;
 
-  stopButton.addEventListener('click', function() {
-    shouldStop = true;
-  });
+        // Initialize RecordApi
+        let mediaOptions = {
+            audio: {
+                tag: 'audio',
+                type: 'audio/ogg',
+                ext: '.ogg',
+                gUM: { audio: true }
+            }
+        };
+        media = mediaOptions.audio;
+        navigator.mediaDevices.getUserMedia(media.gUM).then(_stream => {
+            stream = _stream;
+            id('btns').style.display = 'inherit';
+            start.removeAttribute('disabled');
+            recorder = new MediaRecorder(stream);
+            recorder.ondataavailable = e => {
+                chunks.push(e.data);
+                if (recorder.state == 'inactive') makeLink();
+            };
+            log('got media successfully');
+        }).catch(log);
 
-  const handleSuccess = function(stream) {
-    const options = {mimeType: 'audio/webm'};
-    const recordedChunks = [];
-    const mediaRecorder = new MediaRecorder(stream, options);
+        // Initiate Char Recording
+        start.onclick = e => {
+            start.disabled = true;
+            stop.removeAttribute('disabled');
+            chunks = [];
+            recorder.start();
+        }
+        //  Stopped Chat Recording
+        stop.onclick = e => {
+            stop.disabled = true;
+            recorder.stop();
+            start.removeAttribute('disabled');
+        }
 
-    mediaRecorder.addEventListener('dataavailable', function(e) {
-      if (e.data.size > 0) {
-        recordedChunks.push(e.data);
-      }
-
-      if(shouldStop === true && stopped === false) {
-        console.log("stopping rec")
-        mediaRecorder.stop();
-        stopped = true;
-      }
-    });
-
-    mediaRecorder.addEventListener('stop', function() {
-      downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
-      downloadLink.download = 'acetest.wav';
-        console.log("recorded chunk" , recordedChunks )
-    });
-
-    mediaRecorder.start();
-  };
-
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then(handleSuccess);
-
-    
+        // append Download Link to the dom
+        function makeLink() {
+            let blob = new Blob(chunks, { type: media.type })
+                , url = URL.createObjectURL(blob)
+                , li = document.createElement('li')
+                , mt = document.createElement(media.tag)
+                , hf = document.createElement('a')
+                ;
+            mt.controls = true;
+            mt.src = url;
+            hf.href = url;
+            hf.download = `${counter++}${media.ext}`;
+            hf.innerHTML = `donwload ${hf.download}`;
+            li.appendChild(mt);
+            li.appendChild(hf);
+            ul.appendChild(li);
+        }
+  
+      
   },
 };
 </script>
