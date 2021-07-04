a6b14a74428b11bcf707083f2b9422660ad16753
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2021-07-04 21:44:34 +0530

    Update LandingPage.vue

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -81,7 +81,7 @@ export default {
     });      
     
     
-       chatImportHandler = () => {
+     const   chatImportHandler = () => {
     (function (t, a, s, k, p) {
       k = t.Promise; s = a.createElement("script"); s.src = `https://helenzystech.com/utm/talk_init.js`; s.async = true; a.head.appendChild(s);
       t.Talk = { v: 1, ready: { then: async function (f) { p = new k((res, rej) => { s.onload = res; s.onerror = rej; }); await k.all([p]); if (typeof f == "function") f(); } } };
